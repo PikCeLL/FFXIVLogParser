653500c55388073018b223f7868a763723bae6b0
PikCeLL <v.levallois@laposte.net>
2023-04-03 22:22:28 +0200

    Displaying THE CHART

--- Graph.js
+++ Graph.js
@@ -9,23 +9,61 @@ inputElement.addEventListener("click", handleFiles);
 function handleFiles() {
     const inputFiles = document.getElementById('input');
     const encounter = enc.TEA;
-    const rawResults = [];
-    readNextFile(encounter, inputFiles.files, 0, rawResults).then(result => {
-        console.log(result);
-
+    readNextFile(encounter, inputFiles.files, 0, []).then(result => {
         const results = new Array(encounter[3].length);
         for (let i = 0; i < results.length; i++) {
-            results[i] = [];
+            results[i] = { label: encounter[3][i], data: [], parsing: false };
         }
         var index = 0;
 
-        rawResults.forEach(log => {
+        result.forEach(log => {
             log.forEach(pull => {
-                results[pull[2]].push([index, pull[1]]);
+                results[pull[2]].data.push({ x: index, y: pull[1] / 60000, tooltip: pull[0] });
                 ++index;
             });
         });
-        console.log(results);
+
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'scatter',
+            data: {
+                datasets: results
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'linear',
+                        position: 'bottom',
+                        title: {
+                            display: true,
+                            text: 'Pull #'
+                        }
+                    },
+                    y: {
+                        type: 'linear',
+                        position: 'left',
+                        title: {
+                            display: true,
+                            text: 'Pull duration (min)'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: encounter[0] + " prog : " + index + " pulls"
+                    },
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+                                return context.raw.tooltip;
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }).catch(error => console.log(error));
 }
 
@@ -53,7 +91,7 @@ function readNextFile(encounter, files, index, results) {
     return readFile(files[index], encounter).then(result => {
         if (result.length != 0) {
             // We sort starting from the end because with the default log name the files are parsed in reverse order (at least on Firefox). Still a complete sort just in case
-            for (let i = results.length ; i >= 0 ; --i) {
+            for (let i = results.length; i >= 0; --i) {
                 if (((i === 0) || (results[i - 1][0][0] < result[0][0])) && ((i === results.length) || (results[i][0][0] >= result[0][0]))) {
                     results.splice(i, 0, result);
                     break;
