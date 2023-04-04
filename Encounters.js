d19bd9073a51a7e472d48a9f2686ba904ddb5f9f
PikCeLL <v.levallois@laposte.net>
2023-04-05 00:57:57 +0200

    Adding languages, fight select and progressbar!

--- Encounters.js
+++ Encounters.js
@@ -3,11 +3,46 @@ import * as netEvt from "./NetworkLineEvent.js";
 "use strict";
 
 /* ====ULTIMATES==== */
-export const TEA = ['TEA', '80037586', ["Living Liquid", "Cruise Chaser", "Alexander Prime", "Perfect Alexander"], ["LL", "BJCC", "Prime", "Perfect"]];
-export const UCoB = ['UCoB', '80037569', ["Twintania", "Nael Deus Darnus", "Bahamut Prime", "Twintania", "Bahamut Prime"], ["Twin", "Nael", "Trios", "Adds", "Golden"]];
-export const UwU = ['UwU', '80037573', ["Garuda", "Ifrit", "Titan", "The Ultima Weapon"], ["Garuda", "Ifrit", "Titan", "Ultima"]];
-export const DSR = ['DSR', '8003759A', ["King Thordan", "Nidhogg", "Right Eye", "Ser Charibert", "King Thordan", "Hraesvelgr", "Dragon-king Thordan"], ["King Thordan", "Nidhogg", "Eyes", "Rewind!", "King Thordan II", "Dragons", "The Dragon King"]];
-export const TOP = ['TOP', 'XXXXXXXX', [], []];
+export const UCoB = ['UCoB',
+                     '80037569',
+                     [["Twintania", "Gémellia", "ツインタニア", "双塔尼亚", "트윈타니아"],
+                      ["Nael Deus Darnus", "ネール・デウス・ダーナス", "奈尔·神·达纳斯", "넬 데우스 다르누스"],
+                      ["Bahamut Prime", "Primo-Bahamut", "バハムート・プライム", "至尊巴哈姆特", "바하무트 프라임"],
+                      ["Twintania", "Gémellia", "ツインタニア", "双塔尼亚", "트윈타니아"],
+                      ["Bahamut Prime", "Primo-Bahamut", "バハムート・プライム", "至尊巴哈姆特", "바하무트 프라임"]],
+                     ["Twin", "Nael", "Trios", "Adds", "Golden"]];
+export const UwU = ['UWU',
+                    '80037573',
+                    [["Garuda", "ガルーダ", "迦楼罗", "가루다"],
+                     ["Ifrit", "イフリート", "伊弗利特", "이프리트"],
+                     ["Titan", "タイタン", "泰坦", "타이탄"],
+                     ["The Ultima Weapon", "Ultima Arma", "アルテマウェポン", "究极神兵", "알테마 웨폰"]],
+                    ["Garuda", "Ifrit", "Titan", "Ultima"]];
+export const TEA = ['TEA',
+                    '80037586',
+                    [["Living Liquid", "Liquide Vivant", "リビングリキッド", "有生命活水", "살아있는 액체"],
+                     ["Cruise Chaser", "Croiseur-chasseur", "クルーズチェイサー", "巡航驱逐者", "순항추격기"],
+                     ["Alexander Prime", "Primo-Alexander", "アレキサンダー・プライム", "至尊亚历山大", "알렉산더 프라임"],
+                     ["Perfect Alexander", "Alexander parfait", "パーフェクト・アレキサンダー", "完美亚历山大", "완전체 알렉산더"]],
+                    ["LL", "BJCC", "Prime", "Perfect"]];
+export const DSR = ['DSR',
+                    '8003759A',
+                    [["King Thordan", "Roi Thordan", "騎神トールダン", "骑神托尔丹", "기사신 토르당"],
+                     ["Nidhogg", "ニーズヘッグ", "尼德霍格", "니드호그"],
+                     ["Right Eye", "œil droit de Nidhogg", "邪竜の右眼", "邪龙的右眼", "사룡의 오른눈"],
+                     ["Ser Charibert", "Sire Charibert", "聖騎士シャリベル", "圣骑士沙里贝尔", "성기사 샤리베르"],
+                     ["King Thordan", "Roi Thordan", "騎神トールダン", "骑神托尔丹", "기사신 토르당"],
+                     ["Hraesvelgr", "フレースヴェルグ", "赫拉斯瓦尔格", "흐레스벨그"],
+                     ["Dragon-king Thordan", "Thordan le Dieu Dragon", "騎竜神トールダン", "龙威骑神托尔丹", "기룡신 토르당"]],
+                    ["King Thordan", "Nidhogg", "Eyes", "Rewind!", "King Thordan II", "Dragons", "The Dragon King"]];
+export const TOP = ['TOP',
+                    '800375AC',
+                    [["Omega", "Oméga", "オメガ"],
+                     ["Omega-M", "Oméga-M", "オメガM"],
+                     ["Omega", "Oméga", "オメガ"],
+                     ["Omega-M", "Oméga-M", "オメガM"],
+                     ["Alpha Omega", "Alpha Oméga", "アルファオメガ"]],
+                    ["Omega", "M/F", "Reconfigured + Blue Screen", "Dynamis", "Alpha"]];
 
 
 export class PhasedPullProcessor {
@@ -26,7 +61,7 @@ export class PhasedPullProcessor {
      */
     createNewPull(fightID) {
         if (fightID === this.#encounter[1]) {
-            this.#currentPull = [new Date(1, 1, 1), 0, 0, false];
+            this.#currentPull = [new Date(9999, 12, 31), 0, 0, false];
         } else {
             this.#currentPull = null;
         }
@@ -47,7 +82,7 @@ export class PhasedPullProcessor {
      * @param {Date} endTime 
      */
     wipePull(endTime) {
-        if (this.#currentPull != null) {
+        if (this.#currentPull != null && (endTime > this.#currentPull[0])) {
             this.#currentPull[1] = endTime - this.#currentPull[0];
             this.#pulls.push(this.#currentPull);
             this.#currentPull = null;
@@ -59,7 +94,7 @@ export class PhasedPullProcessor {
      * @param {Date} endTime 
      */
     clearPull(endTime) {
-        if (this.#currentPull != null) {
+        if (this.#currentPull != null && (endTime > this.#currentPull[0])) {
             this.#currentPull[3] = true;
             this.wipePull(endTime);
         }
@@ -70,8 +105,8 @@ export class PhasedPullProcessor {
      * @param {netEvt.NetworkLineEvent} event 
      */
     processEvent(event) {
-        if (this.#currentPull != null && event.type === netEvt.EventType.ABILITY) {
-            if (this.#encounter[2].length > this.#currentPull[2] && this.#encounter[2][this.#currentPull[2] + 1] == event.target) {
+        if (this.#currentPull != null && event.type === netEvt.EventType.ABILITY && event.isSourcePlayer()) {
+            if (this.#encounter[2].length > this.#currentPull[2] + 1 && this.#encounter[2][this.#currentPull[2] + 1].some(s => s.toLowerCase() == event.target.toLowerCase())) {
                 ++this.#currentPull[2];
             }
         }
--- Graph.js
+++ Graph.js
@@ -6,65 +6,112 @@ import * as enc from "./Encounters.js";
 const inputElement = document.getElementById("button");
 inputElement.addEventListener("click", handleFiles);
 
+const ctx = document.getElementById('myChart');
+const chart = new Chart(ctx, {
+    type: 'scatter',
+    data: [],
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Pull #'
+                }
+            },
+            y: {
+                type: 'linear',
+                position: 'left',
+                title: {
+                    display: true,
+                    text: 'Pull duration (min)'
+                }
+            }
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        return context.raw.tooltip;
+                    }
+                }
+            }
+        }
+    }
+});
+
 function handleFiles() {
     const inputFiles = document.getElementById('input');
-    const encounter = enc.TEA;
+    const fightValue = document.getElementById('fights').value;
+    let encounter = enc.UCoB;
+    switch (fightValue) {
+        case "ucob":
+            encounter = enc.UCoB;
+            break;
+        case "uwu":
+            encounter = enc.UwU;
+            break;
+        case "tea":
+            encounter = enc.TEA;
+            break;
+        case "dsr":
+            encounter = enc.DSR;
+            break;
+        case "top":
+            encounter = enc.TOP;
+            break;
+        default:
+            console.log(`${fightValue} doesn't exist!`);
+    }
+
+    document.getElementById('progressBar').max = inputFiles.files.length;
+    document.getElementById('progressBar').value = 0;
+
     readNextFile(encounter, inputFiles.files, 0, []).then(result => {
         const results = new Array(encounter[3].length);
         for (let i = 0; i < results.length; i++) {
             results[i] = { label: encounter[3][i], data: [], parsing: false };
         }
-        var index = 0;
 
+        let index = 0;
+        let combatTime = 0;
         result.forEach(log => {
             log.forEach(pull => {
                 results[pull[2]].data.push({ x: index, y: pull[1] / 60000, tooltip: pull[0] });
+                combatTime += pull[1];
                 ++index;
             });
         });
 
-        const ctx = document.getElementById('myChart');
-
-        new Chart(ctx, {
-            type: 'scatter',
-            data: {
-                datasets: results
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'linear',
-                        position: 'bottom',
-                        title: {
-                            display: true,
-                            text: 'Pull #'
-                        }
-                    },
-                    y: {
-                        type: 'linear',
-                        position: 'left',
-                        title: {
-                            display: true,
-                            text: 'Pull duration (min)'
-                        }
-                    }
-                },
-                plugins: {
-                    title: {
-                        display: true,
-                        text: encounter[0] + " prog : " + index + " pulls"
-                    },
-                    tooltip: {
-                        callbacks: {
-                            label: function(context) {
-                                return context.raw.tooltip;
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        chart.data = { datasets: results };
+        chart.options.plugins.title = {
+            display: true,
+            text: encounter[0] + " prog : " + index + " pulls for " + parseMillisecondsIntoReadableTime(combatTime) + " combat time"
+        };
+        chart.update();
+        console.log(`compteur = ${compteur} || reverseCompteur = ${reverseCompteur}`);
     }).catch(error => console.log(error));
+}
+
+function parseMillisecondsIntoReadableTime(milliseconds) {
+    //Get hours from milliseconds
+    var hours = milliseconds / (1000 * 60 * 60);
+    var absoluteHours = Math.floor(hours);
+    var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+    //Get remainder from hours and convert to minutes
+    var minutes = (hours - absoluteHours) * 60;
+    var absoluteMinutes = Math.floor(minutes);
+    var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes;
+
+    //Get remainder from minutes and convert to seconds
+    var seconds = (minutes - absoluteMinutes) * 60;
+    var absoluteSeconds = Math.floor(seconds);
+    var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+
+    return h + ':' + m + ':' + s;
 }
 
 function readFile(file, encounter) {
@@ -101,5 +148,5 @@ function readNextFile(encounter, files, index, results) {
         return readNextFile(encounter, files, index + 1, results);
     }).catch(error => {
         return Promise.reject(error);
-    }).finally(console.log("File " + index + " read."));
+    }).finally(document.getElementById('progressBar').value = index + 1);
 }
